002352c89e59f2eca04b53dccc3f9c194df6d3bf
Marcus Sen <mase@bgs.ac.uk>
2014-09-23 11:49:00 +0100

    WIP: add catalog iface, impl and test for storing DataURL

---
+++ src/main/src/main/java/org/geoserver/catalog/DataLinkInfo.java
@@ -0,0 +1,53 @@
+/* (c) 2014 Open Source Geospatial Foundation - all rights reserved
+ * (c) 2001 - 2013 OpenPlans
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+package org.geoserver.catalog;
+
+/**
+ * A link to underlying data represented by resource.
+ * 
+ * @author Marcus Sen, British Geological Survey
+ * TODO: check if id and about needed
+ * TODO: where to @uml.property names come from?
+ * TODO: check if it is GeoServer style to re-declare methods of super-interface
+ * 
+ */
+public interface DataLinkInfo extends Info {
+
+    /**
+     * Identifier.
+     */
+    String getId();
+
+    /**
+     * @uml.property name="about"
+     */
+    String getAbout();
+
+    /**
+     * @uml.property name="about"
+     */
+    void setAbout(String about);
+
+    /**
+     * @uml.property name="type"
+     */
+    String getFormat();
+
+    /**
+     * @uml.property name="type"
+     */
+    void setFormat(String type);
+
+    /**
+     * @uml.property name="content"
+     */
+    String getContent();
+
+    /**
+     * @uml.property name="content"
+     */
+    void setContent(String content);
+}
